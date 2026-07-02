9a5038686c19429b282f71854fdf692afa1f9f7f
Rohan Verma <hello@rohanverma.net>
2026-07-02 15:01:20 +0530

    build: use portable Nix Go package

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            go_1_25
+            go
             gopls
             golangci-lint
             just
